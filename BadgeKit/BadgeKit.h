a74c7863e6831e1b2d6ec3131e7ab4cca22bc78c
dvlprliu <dvlprliu@gmail.com>
2018-01-16 18:38:03 +0800

    Initial Commit

---
+++ BadgeKit/BadgeKit.h
@@ -0,0 +1,19 @@
+//
+//  BadgeKit.h
+//  BadgeKit
+//
+//  Created by zhzh liu on 2018/1/16.
+//  Copyright © 2018年 zhzh liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BadgeKit.
+FOUNDATION_EXPORT double BadgeKitVersionNumber;
+
+//! Project version string for BadgeKit.
+FOUNDATION_EXPORT const unsigned char BadgeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BadgeKit/PublicHeader.h>
+
+
